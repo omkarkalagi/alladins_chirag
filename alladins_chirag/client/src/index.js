6027135bdfba0840b35cd4af28f478e0513cf66e
omkarkalagi <omkardigambar4@gmail.com>
2025-07-10 18:13:01 +0530

    Update index.js

--- alladins_chirag/client/src/index.js
+++ alladins_chirag/client/src/index.js
@@ -1,7 +1,7 @@
 // client/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
+import '.alladins_chirag/App.css';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 

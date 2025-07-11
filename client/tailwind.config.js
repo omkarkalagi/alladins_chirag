e97b7baffaca66529bef3ff3c0f8bdcb60a252de
omkarkalagi <omkardigambar4@gmail.com>
2025-07-11 14:01:09 +0530

    qwe

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,23 +1,7 @@
+// tailwind.config.js
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      animation: {
-        shimmer: "shimmer 2s infinite linear"
-      },
-      keyframes: {
-        shimmer: {
-          "0%": { transform: "translateX(-100%)" },
-          "100%": { transform: "translateX(100%)" },
-        }
-      }
-    },
-  },
-  plugins: [],
+  // ...
 }

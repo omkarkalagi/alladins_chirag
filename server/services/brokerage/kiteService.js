a3bb2b0425894e048b204854c488cec7540877f3
omkarkalagi <omkardigambar4@gmail.com>
2025-07-19 19:04:05 +0530

    123

--- server/services/brokerage/kiteService.js
+++ server/services/brokerage/kiteService.js
@@ -1,41 +1,38 @@
 const KiteConnect = require('kiteconnect').KiteConnect;
-const User = require('../../models/User');
 
 const apiKey = process.env.KITE_API_KEY;
-const kite = new KiteConnect({ api_key: apiKey });
+const kite = new KiteConnect({
+  api_key: apiKey
+});
+
+// Store access tokens in DB (simplified)
+let accessToken = '';
 
 module.exports = {
-  getAccessToken: async (userId) => {
-    const user = await User.findById(userId);
-    if (!user || !user.brokerage.accessToken) {
-      throw new Error('Zerodha access token not found');
-    }
-    kite.setAccessToken(user.brokerage.accessToken);
-    return user.brokerage.accessToken;
+  login: (requestToken) => {
+    return kite.generateSession(requestToken, process.env.KITE_API_SECRET)
+      .then(session => {
+        accessToken = session.access_token;
+        kite.setAccessToken(accessToken);
+        return session;
+      });
   },
-
-  generateLoginURL: () => {
-    return kite.getLoginURL();
+  
+  getLiveData: (instruments) => {
+    return kite.getLTP(instruments);
   },
-
-  generateSession: async (requestToken, userId) => {
-    const session = await kite.generateSession(requestToken, process.env.KITE_API_SECRET);
-    await User.findByIdAndUpdate(userId, {
-      'brokerage.accessToken': session.access_token,
-      'brokerage.refreshToken': session.refresh_token
-    });
-    return session;
+  
+  placeOrder: (orderParams) => {
+    return kite.placeOrder(orderParams);
   },
-
-  getLiveData: async (instruments) => {
-    return await kite.getLTP(instruments);
-  },
-
-  placeOrder: async (orderParams) => {
-    return await kite.placeOrder(orderParams);
-  },
-
-  getHistoricalData: async (instrumentToken, interval, from, to) => {
-    return await kite.getHistoricalData(instrumentToken, interval, from, to);
+  
+  getHistoricalData: (symbol, interval) => {
+    return kite.getHistoricalData(
+      `NSE:${symbol}`,
+      interval,
+      new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), // 30 days back
+      new Date(),
+      true
+    );
   }
 };

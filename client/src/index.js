662e1c9142ba47f899cdf6944f3d50ed10810ef0
omkarkalagi <omkardigambar4@gmail.com>
2025-07-20 17:47:42 +0530

    gfgfgf

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// Import all global and component CSS
+import './styles/base.css';
+import './styles/global.css';
+import './styles/main.css';
+import './styles/components.css';
+import './styles/utilities.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,4 +18,5 @@ root.render(
   </React.StrictMode>
 );
 
+// Report web vitals (optional for performance monitoring)
 reportWebVitals();

4066a14629405d53bcf14f01542bbad1cb179dcc
omkarkalagi <omkardigambar4@gmail.com>
2025-07-22 01:46:29 +0530

    123456

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import './styles/global.css';
 import './styles/base.css';
@@ -7,15 +8,14 @@ import './styles/components.css';
 import './styles/utilities.css';
 import './styles/main.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
--- vercel-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf node_modules
-npm ci --legacy-peer-deps
-npm run build
